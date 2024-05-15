da43aae39fa6b9dc9964171915055fe6c6d61826
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-15 21:54:53 +0200

    about page template

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'gray': {
+				1000: "#82827c",
+				1100: "#0000009b",
+				1200: "#000000df",
+			}
+		},
 		extend: {},
 	},
 	plugins: [],
