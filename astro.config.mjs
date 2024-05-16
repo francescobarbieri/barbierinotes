1b5650aa7097b0e5d974f13ac0d197883fb647c1
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-16 12:58:47 +0200

    testing a deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import rehypePrettyCode from 'rehype-pretty-code';
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.barbierinotes.com',
-  integrations: [sitemap(), tailwind(), mdx()]
+  markdown: {
+    extendDefaultPlugins: false,
+    rehypePlugins: [[rehypePrettyCode, {}]]
+  },
+  integrations: [sitemap(), tailwind(), mdx()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
