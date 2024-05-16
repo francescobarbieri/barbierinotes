9c01446bc867123f6933a0746321354b5a574559
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-16 17:36:01 +0200

    testing a deploy, dynamic routes and more

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const postCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.string(),
+        description: z.string(),
+        author: z.string(),
+    })
+});
+
+export const collections = {
+    posts: postCollection,
+}
