968e86c4d00eac7f070ebedc7c0b29aa47e5ec9d
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-17 15:34:07 +0200

    rss fix attempt 1

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,15 +4,15 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
     const posts = await getCollection('posts');
     return rss({
-      title: 'Barbieri Notes',
-      description: "Francesco Barbieri's personal blog.",
-      site: context.site,
-      items: posts.map((post) => ({
-        title: post.data.title,
-        pubDate: post.data.pubDate,
-        description: post.data.description,
-        link: `/posts/${post.slug}/`,
+        title: 'Barbieri Notes',
+        description: "Francesco Barbieri's personal blog.",
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: new Date(post.data.pubDate),
+            description: post.data.description,
+            link: `/posts/${post.slug}/`,
         })),
-      customData: `<language>en-us</language>`,
+        customData: `<language>en-us</language>`,
     });
-  }
+}
