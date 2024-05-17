bc010e08b8dce9abc660e1f3ac3debd5bfda1a74
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-17 16:28:46 +0200

    fixed rss feed locally + improved post sorting function

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const postCollection = defineCollection({
     type: 'content',
     schema: z.object({
         title: z.string(),
-        pubDate: z.string(),
+        publishedDate: z.string(),
         description: z.string(),
     })
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,16 +2,25 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-    const posts = await getCollection('posts');
+    const posts = await getCollection('posts', (post) => {
+        return new Date(post.data.publishedDate) <= new Date();
+    });
+
+    const sortedPosts = posts.sort(
+        (a, b) => new Date(b.data.publishedDate) - new Date(a.data.publishedDate)
+    );
+
     return rss({
         title: "Barbieri Notes",
         description: "Francesco Barbieri's personal blog.",
         site: context.site,
-        items: posts.map((post) => ({
-            title: `${post.data.title}`,
-            pubDate: new Date(post.data.pubDate),
-            description: post.data.description,
-            link: `/posts/${post.slug}/`,
-        })),
+        items: sortedPosts.map((post) => {
+            return ({
+                title: `${post.data.title}`,
+                pubDate: post.data.publishedDate,
+                description: post.data.description,
+                link: `/posts/${post.slug}/`,
+            })
+        }),
     });
 }
