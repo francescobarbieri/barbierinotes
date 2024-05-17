467037a2b1eb6c5252505437c1067c2a6121ea39
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-17 11:51:01 +0200

    fixing deployment errors

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_.._BZK0UpRn.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_MdSGMBII.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/rss_DLORZc93.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/rss_VKZ9cv4T.mjs');
-
-export { page };
