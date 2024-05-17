12cd43118f2209345589352a8290901be2573706
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-05-17 15:58:42 +0200

    rss fix attempt 2

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,15 +4,14 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
     const posts = await getCollection('posts');
     return rss({
-        title: 'Barbieri Notes',
+        title: "Barbieri Notes",
         description: "Francesco Barbieri's personal blog.",
         site: context.site,
         items: posts.map((post) => ({
-            title: post.data.title,
+            title: `${post.data.title}`,
             pubDate: new Date(post.data.pubDate),
             description: post.data.description,
             link: `/posts/${post.slug}/`,
         })),
-        customData: `<language>en-us</language>`,
     });
 }
