0354661688d94db844a5d0d7c563231f0b57e70f
Francesco Barbieri <fbarbieri.business@gmail.com>
2025-05-22 16:21:52 +0200

    header review

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,31 +1,8 @@
 export default function Header() {
   return (
-    <h1 className="font-medium pt-12 transition-element mb-6">
-      <span className="sr-only">Barbieri Notes</span>
-      <span aria-hidden="true" className="block overflow-hidden group relative">
-        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
-          {'Francesco Barbieri'.split('').map((letter, index) => (
-            <span
-              key={index}
-              className="inline-block"
-              style={{ transitionDelay: `${index * 25}ms` }}
-            >
-              {letter === ' ' ? '\u00A0' : letter}
-            </span>
-          ))}
-        </span>
-        <span className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
-          {'Barbieri Notes'.split('').map((letter, index) => (
-            <span
-              key={index}
-              className="inline-block"
-              style={{ transitionDelay: `${index * 25}ms` }}
-            >
-              {letter === ' ' ? '\u00A0' : letter}
-            </span>
-          ))}
-        </span>
-      </span>
-    </h1>
+    <header className="pt-12 mb-8 flex flex-col items-start">
+      <a className="inline-block font-medium no-underline" href="/">Barbieri Notes</a>
+      <a className="flex text-gray-400 no-underline" href="/">Francesco Barbieri</a>
+    </header>
   );
 }
---
+++ src/app/n/post-header.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default function PostHeader() {
+  return (
+    <Link href="/" className="flex mb-8 text-gray-400">
+      Francesco Barbieri
+    </Link>
+  )
+}
