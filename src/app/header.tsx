3ca7c0ed370d3e6fff45be1c48b232d9110b9ecf
Francesco Barbieri <fbarbieri.business@gmail.com>
2025-05-23 21:25:32 +0200

    little header fix at h1

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,7 +6,7 @@ export default function Header({ title = "Barbieri Notes"}) {
   return (
     <header className="pt-12 mb-8 flex flex-col items-start">
       {title == "Barbieri Notes" ? (
-        <a className="inline-block font-medium no-underline" href="/">Barbieri Notes</a>
+        <h1><a className="inline-block font-medium no-underline" href="/">Barbieri Notes</a></h1>
       ) : (
         <h1 className="inline-block font-medium">{title}</h1>
       )}
